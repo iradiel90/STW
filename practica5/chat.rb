6f82adf64a16cea9a491e9e1f114ac143a0120a3
iradiel90 <iradiel90@gmail.com>
2012-12-13 15:19:51 +0000

    practica 5

---
+++ practica5/chat.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+require 'sinatra'
+set server: 'thin', connections: []
+#set connections: []
+
+get '/' do
+	halt erb(:login) unless params[:user]
+	erb :chat, locals: { user: params[:user].gsub(/\W/, '') }
+end
+
+get '/stream', provides: 'text/event-stream' do
+	stream :keep_open do |out|
+		settings.connections << out
+		out.callback { settings.connections.delete(out) }
+	end
+end
+
+# get '/', :provides => ['rss', 'atom', 'xml'] do
+# 	builder :feed
+# end
+
+post '/' do
+	settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+	204 # response without entity body
+end
