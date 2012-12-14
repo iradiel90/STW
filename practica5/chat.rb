d6679fbddba9caebe9989a156f7f7ef322cbafe3
iradiel90 <iradiel90@gmail.com>
2012-12-14 11:21:12 +0000

    terminada

--- practica5/chat.rb
+++ practica5/chat.rb
@@ -1,25 +1,28 @@
-# coding: utf-8
 require 'sinatra'
-set server: 'thin', connections: []
-#set connections: []
+set server: 'thin', connections: {}
 
 get '/' do
-	halt erb(:login) unless params[:user]
-	erb :chat, locals: { user: params[:user].gsub(/\W/, '') }
+  halt erb(:login) if params[:user].nil?
+  erb :chat, locals: { user: params[:user] }
 end
 
-get '/stream', provides: 'text/event-stream' do
-	stream :keep_open do |out|
-		settings.connections << out
-		out.callback { settings.connections.delete(out) }
-	end
+get '/stream/:user', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    settings.connections[params[:user]] = out
+    out.callback { settings.connections.delete params[:user] }
+  end
 end
-
-# get '/', :provides => ['rss', 'atom', 'xml'] do
-# 	builder :feed
-# end
 
 post '/' do
-	settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
-	204 # response without entity body
+  pmsg = /(.+):.*\/(.+):(.*)/
+  msg = pmsg.match(params[:msg])
+  connection = settings.connections[msg[2]] unless msg.nil?
+  if connection.nil?
+    settings.connections.each_pair { |user, out| out << "data: #{params[:msg]}\n\n" }
+    204 # response without entity body
+  else
+    connection << "data: MP de #{msg[1]}: #{msg[3]}\n\n"
+    settings.connections[msg[1]] << "data: MP para #{msg[2]}: #{msg[3]}\n\n"
+    204 # response without entity body
+  end
 end
